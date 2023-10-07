8e8e8e9a63bbab4bac1d672dac9998091d5ca013
Ayush Verma <ayush98277@gmail.com>
2023-10-07 18:12:22 +0530

    message model created

---
+++ Models/MessageModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const messagesSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  }
+});
+
+const messagesModel = mongoose.model("messages", messagesSchema);
+
+module.exports = messagesModel;
--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-const {connection} = require("./Config/config");
+const { connection } = require("./Config/config");
 const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hey, You are on the server");
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Successfully connected to the database");
+  } catch (err) {
+    console.log("Error while connecting to the Database");
+    console.log(err);
+  }
+  console.log(`This server is running at port ${process.env.port}`);
+});
